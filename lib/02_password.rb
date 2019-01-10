cd3a022adb560e3b6fef7b78eb086453db5414ce
joongau <gaultier.jonathan@gmail.com>
2019-01-10 23:35:32 +0100

    dernieres modifs

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,31 +1,30 @@
 def signup
-	puts "Choisis un mot de passe"
-	print ">"
-	mdp = gets.chomp
+    puts "Choisis un mot de passe"
+    print ">"
+    mdp = gets.chomp
 end
 
 def login
-	puts "tape ton mot de passe"
-	print ">"
-	log = gets.chomp
+    puts "tape ton mot de passe"
+    print ">"
+    log = gets.chomp
 end
 
 def welcome_screen
-	mdp = signup
-	log = login
-	if mdp == log
-		puts "Bravo voici les secret de la CIA"
-	end	
-	while mdp != log
-	 	puts "Tpae ton mot de passe"
-		print ">"
-		log = gets.chomp
-	end
-	if mdp == log
-		puts "Bravo voici les secret de la CIA"
-	end
+    mdp = signup
+    log = login
+    while mdp != log
+         puts "Tape ton mot de passe"
+        print ">"
+        log = gets.chomp
+    end
 
-	
+    if mdp == log
+        puts "Bravo voici les secret de la CIA"
+    end
+
+
+
 end
 
 

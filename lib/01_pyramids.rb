c15f705cd09ba4f312e4c19d26aef4bca297bd3f
joongau <gaultier.jonathan@gmail.com>
2019-01-10 23:24:38 +0100

    dernieres modifs

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,48 +1,45 @@
 def number_user
-	puts "Salut, donne moi un nombre entre 1 et 25."
+    puts "Salut, donne moi un nombre entre 1 et 25."
 print ">"
-numberx = gets.chomp
-number = "#{gets.chomp.to_i % 2}"
+number = gets.chomp
 return number
 end
 
 def half_pyramid(number)
-	pyramid = []
-	number.to_i.times do |i|
-	spacea = "#{" " * (number.to_i - i)}"
-	hashtaga = "#{"#" * (i + 1)}"
-	hashtagaa = "#{"#" * i}" 
-	pyramid << spacea + hashtaga + hashtagaa
-	end
-	number.to_i.downto(0) do |i|
-	hashtag = "#{"#" * i}"
-	space = "#{" " * (number.to_i - i)}"
-	hashtagx = "#{"#" * (i + 1)}" 
-	pyramid << space + hashtag + hashtagx
-	end
+    pyramid = []
+    number.to_i.times do |i|
+    spacea = "#{" " * (number.to_i - i)}"
+    hashtaga = "#{"#" * (i + 1)}"
+    hashtagaa = "#{"#" * i}" 
+    pyramid << spacea + hashtaga + hashtagaa
+    end
+    number.to_i.downto(0) do |i|
+    hashtag = "#{"#" * i}"
+    space = "#{" " * (number.to_i - i)}"
+    hashtagx = "#{"#" * (i + 1)}" 
+    pyramid << space + hashtag + hashtagx
+    end
 
-	return pyramid
+    return pyramid
 end
 
 def condition(number)
-	pyramid = half_pyramid(number)
-	if number.to_i >= 26
-		puts "Entre 1 et 25!!"	
-	end
+    pyramid = half_pyramid(number)
+    if number.to_i >= 26
+        puts "Entre 1 et 25!!"
+    end
 
-	if number.to_i <= 25
-		puts pyramid		
-	end
-	
+    if number.to_i <= 25
+        puts pyramid
+    end
+
 end
 
 def perform
-	number = number_user
-	condition = condition(number)
-	half_pyramid(number)
-		
-end	
+    number = number_user
+    condition = condition(number)
+    half_pyramid(number)
+
+end
 
 perform
-
-
